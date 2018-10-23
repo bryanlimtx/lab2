e8220101078a9efa64881db009e3f0609862c0a7
bryanlimtx <limtx-wa15@student.tarc.edu.my>
2018-10-23 16:02:13 +0800

    lab2

---
+++ app/src/main/java/com/example/taruc/myapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.taruc.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("dddd", "dddd");
+        Log.w("warning","hello this is my warning level");
+        Log.e("www","wwww");
+        Log.i("iiii","iii");
+    }
+}
